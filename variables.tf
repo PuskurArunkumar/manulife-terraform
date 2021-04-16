22d4d764024251ac9c295bd21061d8310c705c9e
PuskurArunkumar <51536347+PuskurArunkumar@users.noreply.github.com>
2021-04-16 11:39:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,21 @@
 #Configure Azure Provider and declare all the variables that will be use
 
+variable "subscription_id" {
+   description = " Enter Azure Subscription ID "
+}
+
+variable "tenant_id" {
+   description = " Enter Active Directory Tenant ID "
+}
+
+variable "client_id" {
+   description = " Enter Service Principle ID "
+}
+
+variable "client_secret" {
+   description = " Enter Service Principle Secret "
+}
+
 variable "resourcegroup_name" {
    description = "Resource GroupName"
 }
